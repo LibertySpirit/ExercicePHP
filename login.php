ce284b0c0f36c586ef9be35fb7c651a19cb69173
Miguel <pro.info.lmm@gmail.com>
2018-10-11 12:17:32 +0200

    Deplace: fn_displayform -> en-tete & CorrecBug: login

--- login.php
+++ login.php
@@ -9,52 +9,12 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     do_post();
 }
 
-
-var_dump($_POST);
 // Controler for get
 function do_get() {
 	$user = array_key_exists("user", $_SESSION) ? $_SESSION["user"] : null;
 	$login = null;
 	$msg = null;
 	display_form($user, $login, $msg);
-}
-
-// View
-function display_form($user, $login, $msg) {
-	?>
-	<html>
-		<head>
-			<style>
-				.error {
-					color: red;
-				}
-			</style>
-		</head>
-		<body>
-			<form method="POST">
-				<?php
-				if ($user != null) {
-					?>
-					<button type="submit" name="action" value="disconnect">Disconnect
-						<?= $user["email"] ?></button>
-					<?php
-				} else {
-					?>
-					Login: <input type="text" name="email" value="<?= $login ?>"/>
-					Password: <input type = "password" name = "password"/>
-					<button type="submit">Connect</button>
-					<?php
-					if ($msg != null) {
-					?>	
-					<span class="error"><?= $msg ?></span>
-					<?php
-					}
-				}
-				?>
-			</form>
-		</body>
-	</html>
-	<?php
 }
 
 // Controler for post
--- vue/en-tete.php
+++ vue/en-tete.php
@@ -6,3 +6,40 @@
 	<body class="flex-center">
 <header>Ici le menu commun à toutes les pages</header>
 <hr/>
+<?php
+function display_form($user, $login, $msg) {
+	?>
+	<html>
+		<head>
+			<style>
+				.error {
+					color: red;
+				}
+			</style>
+		</head>
+		<body>
+			<form method="POST">
+				<?php
+				if ($user != null) {
+					?>
+					<button type="submit" name="action" value="disconnect">Disconnect
+						<?= $user["email"] ?></button>
+					<?php
+				} else {
+					?>
+					Login: <input type="text" name="email" value="<?= $login ?>"/>
+					Password: <input type = "password" name = "password"/>
+					<button type="submit">Connect</button>
+					<?php
+					if ($msg != null) {
+					?>	
+					<span class="error"><?= $msg ?></span>
+					<?php
+					}
+				}
+				?>
+			</form>
+		</body>
+	</html>
+	<?php
+} ?>
